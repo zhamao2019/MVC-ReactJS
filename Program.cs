2d690da97b06fb7bacbd63643784066b5c07fced
yang0287 <yangbingdeyouxiang@126.com>
2022-03-28 18:46:41 -0400

    add packages and setup react Js

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+
+builder.Services.AddJsEngineSwitcher(options => 
+    options.DefaultEngineName = V8JsEngine.EngineName).AddV8();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -10,6 +22,9 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseReact(config => { });
+
 app.UseStaticFiles();
 
 app.UseRouting();
